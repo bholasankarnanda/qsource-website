021a74100faa5bb7b6ddc0c6b1471a61b5a62b94
sahil-chandrakar <chandrakarsahil2002@gmail.com>
2025-12-10 14:59:41 +0530

    added find-job

---
+++ src/app/findjobs/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import JobHeroSection from "@/components/jobs/JobHeroSection";
+import RecruitingSection from "@/components/jobs/RecruitingSection";
+import JobOpeningsSection from "@/components/jobs/JobOpeningsSection";
+import ContactSection from "@/components/jobs/ContactSection";
+import { Box, Divider } from "@mui/material";
+
+const findjobs = () => {
+  return (
+    <Box sx={{ fontFamily: "Be Vietnam Pro", fontWeight: 800 }}>
+      <JobHeroSection />
+      <Divider sx={{ my: 2 }} />
+      <RecruitingSection />
+      <Divider sx={{ my: 2 }} />
+      <JobOpeningsSection />
+      <ContactSection />
+    </Box>
+  );
+};
+export default findjobs;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,12 @@ import Footer from "../components/Footer";
 import { Box, Toolbar, Container } from "@mui/material";
 import ThemeRegistry from "../components/ThemeRegistry/ThemeRegistry";
 
+import { Be_Vietnam_Pro } from "next/font/google";
+const beVietnam = Be_Vietnam_Pro({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+});
+
 export const metadata: Metadata = {
   title: "QSource Landing",
   description: "Enterprise IT Services",
